3cb74e1c62d5019e2738453a8a7d0b7f108b489e
Robert Nica <rnica@biobam.com>
2024-11-07 13:39:49 +0100

    Add whenInputNotNull condition

--- src/lib/Conditional.ts
+++ src/lib/Conditional.ts
@@ -23,12 +23,27 @@ export class Conditional extends Construct {
     this._expression = js;
   }
 
+  /**
+   * 
+   * @param input A boolean step input parameter.
+   * @param value <code>true</code> to run the step only if the input is true, <code>false</code> to run the step only if the input is false.
+   */
   whenBooleanInputIs(input: Input, value: boolean) {
     // Only accept this method if the input is a boolean
     if (input.type != Type.BOOLEAN) {
       throw new Error(`Input ${input.id} is not a boolean. Conditional can only be applied on boolean inputs.`);
     }
-    this._expression = '$(inputs.' + input.id + ' == ' + value + ')';
+    this._expression = `$(inputs.${input.id} == ${value})`;
+  }
+
+  /**
+   * Skip this step if the specified input is null.
+   * 
+   * @param input One of the step input parameters.
+   */
+  whenInputNotNull(input: Input) {
+    // Check if the referenced input is part of the step.
+    this._expression = `$(inputs.${input.id} !== null)`;
   }
 
 }
